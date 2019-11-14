140a657d1dcca38f12a14f292dfb621029848865
Irina Kagan <irinagamb@gmail.com>
2019-11-14 21:05:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,84 @@
 /* Your Code Here */
 
+
+function createEmployeeRecord(arr){
+  const testEmployee ={
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return testEmployee
+}
+function createEmployeeRecords(arr){
+
+return arr.map(createEmployeeRecord)
+}
+
+let createTimeInEvent = function(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
+
+let createTimeOutEvent = function(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
+
+function hoursWorkedOnDate(employee, time){
+
+  const timeIn = employee.timeInEvents[0]
+  const timeOut = employee.timeOutEvents[0]
+  const hoursWorked = Math.abs(timeIn.hour - timeOut.hour)/100
+  return hoursWorked
+}
+
+
+
+function wagesEarnedOnDate(employee, day){
+
+  const payPerHour = employee.payPerHour
+  const timeIn = employee.timeInEvents[0]
+  const timeOut = employee.timeOutEvents[0]
+
+
+  const hoursWorked = Math.abs(timeIn.hour - timeOut.hour)/100
+  const wagesEarned =  payPerHour * hoursWorked
+  return wagesEarned
+}
+
+
+
+
+function calculatePayroll(employees){
+  return employees.reduce((totalPay, employee)=> {
+     totalPay += allWagesFor(employee)
+     return totalPay
+  }, 0)
+}
+
+function findEmployeeByFirstName(employees){
+  const firstName = employees.find(employee => employee.firstName === 'Loki')
+  return firstName
+
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
